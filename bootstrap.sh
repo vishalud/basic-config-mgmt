e3510cf8e96134c853283d5d7b6d49bd336e24fb
Vishal Uderani <vuderani@opentable.com>
2021-06-29 14:31:27 +0530

    Add missing flag to apt to automate install for needsrestart and remove package not needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Install needrestart that checks daemons need to be restarted after library upgrades/changes
-sudo apt-get install needrestart
+sudo apt-get install -y needrestart
