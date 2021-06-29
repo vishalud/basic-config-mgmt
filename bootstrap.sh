7629e6248775e3eb65d99b58768cc81dd0f20d38
Vishal Uderani <vuderani@opentable.com>
2021-06-29 14:53:27 +0530

    run apt-get update before starting the setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+#Run apt-get update as we cant find the needrestart package otherwise
+sudo apt-get update -y
 # Install needrestart that checks daemons need to be restarted after library upgrades/changes
 sudo apt-get install -y needrestart
